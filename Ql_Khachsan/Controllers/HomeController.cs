a6b59fbed83d828f35a1ac1a38b5f3ed7855dbc6
kanekikenhn@gmail.com <kanekikenhn@gmail.com>
2022-04-09 09:48:30 +0700

    first commit

---
+++ Ql_Khachsan/Controllers/HomeController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ql_Khachsan.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Gallery()
+        {
+            return View();
+        }
+        public ActionResult BlogGrid()
+        {
+            return View();
+        }
+        public ActionResult BlogList()
+        {
+            return View();
+        }
+        public ActionResult BlogDetail()
+        {
+            return View();
+        }
+        public ActionResult Services()
+        {
+            return View();
+        }
+        public ActionResult ServiceDetail()
+        {
+            return View();
+        }
+        public ActionResult Dinnings()
+        {
+            return View();
+        }
+        public ActionResult FAQ()
+        {
+            return View();
+        }
+        public ActionResult LuxuryTours()
+        {
+            return View();
+        }
+        public ActionResult PackagesGrid()
+        {
+            return View();
+        }
+        public ActionResult PackagesList()
+        {
+            return View();
+        }
+        public ActionResult PackagesDetail()
+        {
+            return View();
+        }
+        public ActionResult AllFlight()
+        {
+            return View();
+        }
+        public ActionResult FlightDetail()
+        {
+            return View();
+        }
+        public ActionResult BookingForm()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ Ql_Khachsan/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Ql_Khachsan.MvcApplication" Language="C#" %>
